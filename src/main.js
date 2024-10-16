7c1479600c9144f8d1bd636babb24b576f22dfb2
cendyz <arek57823@gmail.com>
2024-10-16 16:56:55 +0200

    java update

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ const amountInput = document.querySelector("#amount");
 const termInput = document.querySelector("#term");
 const rateInput = document.querySelector("#rate");
 const inputs = [amountInput, rateInput, termInput];
-
+const btn = document.querySelector(".main__one-calculate");
+const clearBtn = document.querySelector(".main__one-btn");
+const exceptComa = /^\d+(,\d+)*$/;
+const exceptDot = /^\d+(.\d+)*$/;
+const numbersOnly = /^\d+$/;
 const checkBox = e => {
 	const index = Array.from(checkBoxes).indexOf(e.target);
 	checkMain[index].style.backgroundColor = "#eff0c8";
@@ -25,12 +29,62 @@ const checkChecked = () => {
 	});
 };
 
+const clearAll = () => {
+	inputs.forEach(el => {
+		el.value = "";
+		el.nextElementSibling.style.backgroundColor = "hsl(202, 86%, 94%)";
+		el.nextElementSibling.style.color = "hsl(200, 24%, 40%)";
+	});
+
+	resetCheckboxes();
+	checkChecked();
+};
+
+const checkInputs = (input, regex) => {
+	if (regex.test(input.value)) {
+		input.nextElementSibling.style.backgroundColor = "hsl(202, 86%, 94%)";
+		input.nextElementSibling.style.color = "hsl(200, 24%, 40%)";
+	} else {
+		input.nextElementSibling.style.backgroundColor = "rgba(215,52,41,255)";
+		input.nextElementSibling.style.color = "white";
+	}
+};
+
 checkBoxes.forEach(box => box.addEventListener("click", checkBox));
 
 resetCheckboxes();
+
+btn.addEventListener("click", e => {
+	e.preventDefault();
+	checkInputs(amountInput, exceptComa);
+	checkInputs(termInput, numbersOnly);
+	checkInputs(rateInput, exceptDot);
+
+	// console.log(rateInput.value + termInput.value);
+});
+
+clearBtn.addEventListener("click", clearAll);
 
 window.addEventListener("DOMContentLoaded", () => {
 	inputs.forEach(input => {
 		input.value = "";
 	});
+});
+
+amountInput.addEventListener("input", function () {
+	let value = this.value.replace(/,/g, "");
+	if (value.length > 3) {
+		value = value.slice(0, 3) + "," + value.slice(3);
+	}
+	this.value = value;
+
+	const numericValue = parseFloat(this.value.replace(/,/g, ""));
+});
+
+rateInput.addEventListener("input", function () {
+	let value = this.value.replace(/\./g, "");
+	if (value.length > 1) {
+		value = value.slice(0, 1) + "." + value.slice(1);
+	}
+	this.value = value;
 });
